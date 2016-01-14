7c172b3aeb6ba80da8dd8a378720f9db5069236f
Roman Treutlein <Roman Treutlein>
2016-01-14 17:19:34 +0800

    Updated to next commit of Tersmu Fixed issue with the install script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-libname=$(stack query | awk 'NR==8' | sed 's/://g'| sed 's/ //g')
-libver=$(stack query | awk 'NR==10' | sed 's/version: //g' | sed "s/'//g" | sed "s/ //g")
+libname=$(stack query | awk 'NR==5' | sed 's/://g'| sed 's/ //g')
+libver=$(stack query | awk 'NR==7' | sed 's/version: //g' | sed "s/'//g" | sed "s/ //g")
 
 sudo rm /usr/local/lib/opencog/"lib$libname-$libver.so"
 
